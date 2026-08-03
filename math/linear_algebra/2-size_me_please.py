4fe853a668f5e0b04a388affad9a0199ea662bd1
Khosuke <frederic.iglesias.montero@gmail.com>
2026-08-03 12:02:45 +0200

    Add comments to function and main file for tests

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
+    """
+    Function that calculates the shape of a matrix.
+    """
     if not isinstance(matrix, list):
         return []
     return [len(matrix)] + matrix_shape(matrix[0])
