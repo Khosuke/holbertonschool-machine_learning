d93ef051b2b30810134617e93273bb3852f660d3
Khosuke <frederic.iglesias.montero@gmail.com>
2026-08-05 11:40:26 +0200

    Task 9 Write a function def summation_i_squared(n): that calculates sum of the first n consecutive square numbers

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+This module implements one function def summation_i_squared(n)
+"""
+
+
+def summation_i_squared(n):
+    """
+    Function that calculates the sum of 
+    the first n consecutive square numbers
+    Args:
+        n: the stopping condition number
+    """
+    if not isinstance(n, int):
+        return None
+    return int((n*(n+1)*(2*n+1))/6)
