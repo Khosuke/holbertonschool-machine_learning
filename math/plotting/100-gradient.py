9352471ced0306e6d30068a5559057fff80cef61
Khosuke <frederic.iglesias.montero@gmail.com>
2026-08-07 11:42:20 +0200

    Remove xtick, ytick, xlim and ylim because they are not required

--- math/plotting/100-gradient.py
+++ math/plotting/100-gradient.py
@@ -16,11 +16,8 @@ def gradient():
     x = np.random.randn(2000) * 10
     y = np.random.randn(2000) * 10
     z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
+    plt.figure(figsize=(6.4, 4.8))
 
-    plt.xlim(-40, 40)
-    plt.ylim(-40, 40)
-    plt.xticks(range(-30, 31, 10))
-    plt.yticks(range(-30, 31, 10))
     plt.xlabel("x coordinate (m)")
     plt.ylabel("y coordinate (m)")
     plt.title("Mountain Elevation")
