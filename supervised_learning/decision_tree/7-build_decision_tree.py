82101a4618494979e7fe144081fec07d91e61d9c
Khosuke <frederic.iglesias.montero@gmail.com>
2026-08-13 09:46:30 +0200

    Add tabspaces in fit method print output

--- supervised_learning/decision_tree/7-build_decision_tree.py
+++ supervised_learning/decision_tree/7-build_decision_tree.py
@@ -337,11 +337,11 @@ class Decision_Tree():
         self.update_predict()    # <--- defined in the previous task
 
         if verbose == 1:
-                print(f"""  Training finished.
-- Depth                     : { self.depth()       }
-- Number of nodes           : { self.count_nodes() }
-- Number of leaves          : { self.count_nodes(only_leaves=True) }
-- Accuracy on training data : { self.accuracy(self.explanatory, self.target)    }""")
+            print(f"""  Training finished.
+    - Depth                     : { self.depth()       }
+    - Number of nodes           : { self.count_nodes() }
+    - Number of leaves          : { self.count_nodes(only_leaves=True) }
+    - Accuracy on training data : { self.accuracy(self.explanatory, self.target)    }""")
 
     def np_extrema(self, arr):
         """
