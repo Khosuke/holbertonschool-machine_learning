8ad7f9c0ef7dc010886b6c4385855902a67e188f
Khosuke <frederic.iglesias.montero@gmail.com>
2026-09-18 09:53:14 +0200

    Task 9. Privatize NeuralNetwork, make the attributes private with getter functions

---
+++ supervised_learning/classification/9-neural_network.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""
+This module defines a single class NeuralNetwork
+"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """
+    This class defines a neural network with one hidden
+    layer performing binary classification.
+    Attributes:
+        W1: The weights vector for the hidden layer.
+        b1: The bias for the hidden layer.
+        A1: The activated output for the hidden layer.
+        W2: The weights vector for the output neuron.
+        b2: The bias for the output neuron.
+        A2: The activated output for the output neuron (prediction).
+    """
+    def __init__(self, nx, nodes):
+        """
+        Class constructor
+        Args:
+            nx: number of input features
+            nodes: number of nodes found in the hidden layer
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        return self.__W1
+
+    @property
+    def b1(self):
+        return self.__b1
+
+    @property
+    def A1(self):
+        return self.__A1
+
+    @property
+    def W2(self):
+        return self.__W2
+
+    @property
+    def b2(self):
+        return self.__b2
+
+    @property
+    def A2(self):
+        return self.__A2
